5c2909b951cc5e6d783be3e8ec82ca6131c9e935
smb602 <smb602@cs.bham.ac.uk>
2018-08-03 13:49:04 +0100

    Created a textfield inside UIAlert to add new entries to pain tracker

--- Someone Like Me/Controller/PainTrackerTableViewController.swift
+++ Someone Like Me/Controller/PainTrackerTableViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class PainTrackerTableViewController: UITableViewController {
 
-    let painItemArray = ["Monday", "Tuesday", "Wednesday"]
+    var painItemArray = ["Monday", "Tuesday", "Wednesday"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,8 +22,8 @@ class PainTrackerTableViewController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
-    // MARK: - Methods for Table view data source
     
+    // MARK: - Methods for Table view data source
     //data source method, creating cells in the table view
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return painItemArray.count
@@ -38,6 +38,7 @@ class PainTrackerTableViewController: UITableViewController {
         return cell
     }
     
+    
     // MARK: - Methods for tableview delegate
     //tells the delegate the specified rows selected
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -51,10 +52,32 @@ class PainTrackerTableViewController: UITableViewController {
         }
         
         tableView.deselectRow(at: indexPath, animated: true) //deselects row after clicked so it doesn't stay grey ui improvement 
-        
-        
+        }
     
+    
+    // MARK - Methods for adding new pain items to the pain tracker
+    @IBAction func additionButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var localTextField = UITextField() //this local varaible within the IBaction is available to all in additionButtonPressed method
+        
+        //UI alert and append to end of my painItemArray
+        let thisAlert = UIAlertController(title: "Add New Pain Date", message: "", preferredStyle: .alert)
+        
+        //this will occur once a user selects the add icon on the UIAlert
+        let action = UIAlertAction(title: "Add Entry", style: .default) { (action) in
+            self.painItemArray.append(localTextField.text!) //inside closure (in keyword) specify self so compiler knows where it exists in this class
+            
+            self.tableView.reloadData() // so new data added to item array appears on tableview
+        }
+        thisAlert.addTextField { (thisAlertTextField) in
+            thisAlertTextField.placeholder = "Enter the date" //placeholder within textfield to enter new additions
+            localTextField = thisAlertTextField //extending scope of this textfield in the closure
+        }
+        
+        thisAlert.addAction(action) //adding action to alert
+        present(thisAlert, animated: true, completion: nil) //showing alert
+        
     }
-
+    
     
 }
